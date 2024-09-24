4f39c1e7f7b5574980d5ef976a918d4cf32e688f
shangcom <dndbs91@gmail.com>
2024-09-24 21:03:45 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -0,0 +1,61 @@
+package hello.itemservice.web.form;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/form/items") // 요청 경로. 절대 경로처럼 작용. 이 클래스의 모든 메서드가 기본으로 사용.
+@RequiredArgsConstructor
+public class FormItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "form/items"; // 뷰 경로. 상대 경로로 해석. 뷰 리졸버가 이 경로를 기준으로 실제 뷰 파일을 찾음.
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "form/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "form/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/form/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "form/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String editV1(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/form/items/{itemId}";
+    }
+
+}
